7a8d6420706b93414e97c5349176b8817151ae4b
benzaku <benzaku@gmail.com>
2015-08-20 22:52:38 +0200

    create project.

---
+++ PokerHands/src/com/flaregame/interview/pokerhands/model/Card.java
@@ -0,0 +1,23 @@
+package com.flaregame.interview.pokerhands.model;
+
+public class Card {
+	private Integer value;
+	private Suit suit;
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+	public void setSuit(Suit suit) {
+		this.suit = suit;
+	}
+	
+}
---
+++ PokerHands/src/com/flaregame/interview/pokerhands/model/Suit.java
@@ -0,0 +1,5 @@
+package com.flaregame.interview.pokerhands.model;
+
+public enum Suit {
+	SPADE, HEART, CLUB, DIAMONDS;
+}
